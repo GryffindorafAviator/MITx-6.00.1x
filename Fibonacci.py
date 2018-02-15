2b7a34a61ba559d77e886bceaaf03b12fb04cc20
Flash_W <phynew@hotmail.com>
2018-02-16 01:24:45 +0800

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,10 @@
+def fib(x):
+    """assumes x an int >= 0
+    returns Fibonacci of x"""
+
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x-1) + fib(x-2)
+
+print(fib(5))
