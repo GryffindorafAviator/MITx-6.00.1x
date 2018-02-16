e6959f6bb39ee7e805f0f726929b0208bf11fe82
Flash_W <phynew@hotmail.com>
2018-02-16 17:33:29 +0800

    Create isPalindrome.py

---
+++ isPalindrome.py
@@ -0,0 +1,20 @@
+def isPalindrome(s):
+    def toChars(s):
+        s = s.lower()
+        ans = ''
+
+        for c in s:
+            if c in 'abcdefghijklmnopqrstuvwxyz':
+                ans = ans + c
+        print(ans)
+        return ans
+
+    def isPal(s):
+        if len(s) <= 1:
+            return True
+        else:
+            return s[0] == s[-1] and isPal(s[1:-1])
+
+    return isPal(toChars(s))
+
+print(isPalindrome('Able was I, ere I saw Elba'))
