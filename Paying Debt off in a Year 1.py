8a7910ae69f992d09ae88e349d6e6d3adfd21798
Flash_W <phynew@hotmail.com>
2018-02-17 18:44:30 +0800

    Update and rename Paying Debt off in a Year.py to Paying Debt off in a Year 1.py

---
+++ Paying Debt off in a Year 1.py
@@ -0,0 +1,32 @@
+# Problem 1 - Paying Debt off in a Year
+
+# (10/10 points)
+# Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the
+# credit card company each month.
+
+# The following variables contain values as described below:
+# balance - the outstanding balance on the credit card
+# annualInterestRate - annual interest rate as a decimal
+# monthlyPaymentRate - minimum monthly payment rate as a decimal
+
+# For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining
+# balance. Be sure to print out no more than two decimal digits of accuracy - so print
+
+# Remaining balance: 813.41
+# instead of
+# Remaining balance: 813.4141998135
+
+# So your program only prints out one thing: the remaining balance at the end of the year in the format:
+# Remaining balance: 4784.0
+
+# A summary of the required math is found below:
+# Monthly interest rate= (Annual interest rate) / 12.0
+# Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
+# Monthly unpaid balance = (Previous balance) - (Minimum monthly payment)
+# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+
+for i in range(1, 13):
+    balance = balance * (1 - monthlyPaymentRate) * (1 + annualInterestRate/12.0)
+    ans = round(balance, 2)
+
+print(ans)
--- Paying Debt off in a Year.py
+++
@@ -1,6 +0,0 @@
-# Paste your code into this box
-for i in range(1, 13):
-    balance = balance * (1 - monthlyPaymentRate) * (1 + annualInterestRate/12.0)
-    ans = round(balance, 2)
-
-print(ans)
