02bd0401ed01b3c055f62c458fba20b390d0e278
Flash Wang <phynew0111@gmail.com>
2018-04-09 14:18:15 +0800

    Update Week 5 Problem 1 - Build the Shift Dictionary and Apply Shift.py

--- Week 5 Problem 1 - Build the Shift Dictionary and Apply Shift.py
+++ Week 5 Problem 1 - Build the Shift Dictionary and Apply Shift.py
@@ -1,3 +1,70 @@
+# Problem 1 - Build the Shift Dictionary and Apply Shift
+# 20/20 points (graded)
+# The Message class contains methods that could be used to apply a cipher to a string, either to encrypt or to decrypt a message (since
+# for Caesar codes this is the same action).
+
+# In the next two questions, you will fill in the methods of the Message class found in ps6.py according to the specifications in the
+# docstrings. The methods in the Message class already filled in are:
+# __init__(self, text)
+# The getter method get_message_text(self)
+# The getter method get_valid_words(self), notice that this one returns a copy of self.valid_words to prevent someone from mutating the
+# original list.
+
+# In this problem, you will fill in two methods:
+# Fill in the build_shift_dict(self, shift) method of the Message class. Be sure that your dictionary includes both lower and upper case
+# letters, but that the shifted character for a lower case letter and its uppercase version are lower and upper case instances of the
+# same letter. What this means is that if the original letter is "a" and its shifted value is "c", the letter "A" should shift to the
+# letter "C".
+
+# If you are unfamiliar with the ordering or characters of the English alphabet, we will be following the letter ordering displayed by
+# string.ascii_lowercase and string.ascii_uppercase:
+
+# >>> import string
+# >>> print(string.ascii_lowercase)
+# abcdefghijklmnopqrstuvwxyz
+# >>> print(string.ascii_uppercase)
+# ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+# A reminder from the introduction page - characters such as the space character, commas, periods, exclamation points, etc will not be
+# encrypted by this cipher - basically, all the characters within string.punctuation, plus the space (' ') and all numerical characters
+# (0 - 9) found in string.digits.
+
+# Fill in the apply_shift(self, shift) method of the Message class. You may find it easier to use build_shift_dict(self, shift). Remember
+# that spaces and punctuation should not be changed by the cipher.
+
+class Message(object):
+    ### DO NOT MODIFY THIS METHOD ###
+    def __init__(self, text):
+        '''
+        Initializes a Message object
+                
+        text (string): the message's text
+
+        a Message object has two attributes:
+            self.message_text (string, determined by input text)
+            self.valid_words (list, determined using helper function load_words
+        '''
+        self.message_text = text
+        self.valid_words = load_words(WORDLIST_FILENAME)
+
+    ### DO NOT MODIFY THIS METHOD ###
+    def get_message_text(self):
+        '''
+        Used to safely access self.message_text outside of the class
+        
+        Returns: self.message_text
+        '''
+        return self.message_text
+
+    ### DO NOT MODIFY THIS METHOD ###
+    def get_valid_words(self):
+        '''
+        Used to safely access a copy of self.valid_words outside of the class
+        
+        Returns: a COPY of self.valid_words
+        '''
+        return self.valid_words[:]
+        
     def build_shift_dict(self, shift):
         '''
         Creates a dictionary that can be used to apply a cipher to a letter.
@@ -12,34 +79,56 @@
         Returns: a dictionary mapping a letter (string) to 
                  another letter (string). 
         '''
-        plaintext_list = []
-        self.ciphertext_list = []
+        plaintext_list_lowercase = []
+        plaintext_list_uppercase = []
+        self.ciphertext_list_lowercase = []
+        self.ciphertext_list_uppercase = []
+        self.shift_dict = {}
 
-        for letter in string.ascii_lowercase:
-            plaintext_list.append(letter)
+        for let in string.ascii_lowercase:
+            plaintext_list_lowercase.append(let)
+
+        for ter in string.ascii_uppercase:
+            plaintext_list_uppercase.append(ter)
 
         for i in range(0, 26 - shift):
-            self.ciphertext_list.append(plaintext_list[i + shift])
+            self.ciphertext_list_lowercase.append(plaintext_list_lowercase[i + shift])
+            self.ciphertext_list_uppercase.append(plaintext_list_uppercase[i + shift])
 
-        for i in range(-3, 0):
-            self.ciphertext_list.append(plaintext_list[i + shift])
+        for j in range(-shift, 0):
+            self.ciphertext_list_lowercase.append(plaintext_list_lowercase[j + shift])
+            self.ciphertext_list_uppercase.append(plaintext_list_uppercase[j + shift])
 
-        return self.ciphertext_list
+        plaintext_list = plaintext_list_lowercase + plaintext_list_uppercase
+        ciphertext_list = self.ciphertext_list_lowercase + self.ciphertext_list_uppercase
+
+        self.shift_dict = dict(zip(plaintext_list, ciphertext_list))
+
+        return self.shift_dict
+
+    def apply_shift(self, shift):
+        '''
+        Applies the Caesar Cipher to self.message_text with the input shift.
+        Creates a new string that is self.message_text shifted down the
+        alphabet by some number of characters determined by the input shift        
         
-import string
+        shift (integer): the shift with which to encrypt the message.
+        0 <= shift < 26
 
-plaintext_list = []
-ciphertext_list = []
-shift = 3
+        Returns: the message text (string) in which every character is shifted
+             down the alphabet by the input shift
+        '''
+        self.shift_string = ""
+        original_string = self.get_message_text()
+        shift_dict = self.build_shift_dict(shift)
 
-for letter in string.ascii_lowercase:
-    plaintext_list.append(letter)
 
-for i in range(0, 26 - shift):
-     ciphertext_list.append(plaintext_list[i + shift])
+        for letter in original_string:
+            if letter not in string.punctuation and letter not in string.digits and letter != " ":
+                for key in self.shift_dict.keys():
+                    if letter == key:
+                        self.shift_string += shift_dict[key]
+            else:
+                self.shift_string += letter
 
-for i in range(-3, 0):
-    ciphertext_list.append(plaintext_list[i+shift])
-
-print(plaintext_list)
-print(ciphertext_list) 
+        return self.shift_string
