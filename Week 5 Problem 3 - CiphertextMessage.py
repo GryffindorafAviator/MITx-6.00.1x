c2e18a32f3d05714ee9fa54e3dd05d858c778fcf
Flash Wang <phynew0111@gmail.com>
2018-08-07 22:45:18 +0800

    Update Week 5 Problem 3 - CiphertextMessage.py

--- Week 5 Problem 3 - CiphertextMessage.py
+++ Week 5 Problem 3 - CiphertextMessage.py
@@ -1,3 +1,24 @@
+# Problem 3 - CiphertextMessage
+# 15/15 points (graded)
+# For this problem, the graders will use our implementation of the Message and PlaintextMessage classes, so don't worry if you did not
+# get the previous parts correct.
+
+# Given an encrypted message, if you know the shift used to encode the message, decoding it is trivial. If message is the encrypted
+# message, and s is the shift used to encrypt the message, then apply_shift(message, 26-s) gives you the original plaintext message. Do 
+# you see why?
+
+# The problem, of course, is that you don’t know the shift. But our encryption method only has 26 distinct possible values for the shift!
+# We know English is the main language of these emails, so if we can write a program that tries each shift and maximizes the number of
+# English words in the decoded message, we can decrypt their cipher! A simple indication of whether or not the correct shift has been
+# found is if most of the words obtained after a shift are valid words. Note that this only means that most of the words obtained are
+# actual words. It is possible to have a message that can be decoded by two separate shifts into different sets of words. While there are
+# various strategies for deciding between ambiguous decryptions, for this problem we are only looking for a simple solution.
+
+# Fill in the methods in the class CiphertextMessage acording to the specifications in ps6.py. The methods you should fill in are:
+# __init__(self, text): Use the parent class constructor to make your code more concise.
+# decrypt_message(self): You may find the helper function is_word(wordlist, word) and the string method split() useful. Note that is_word
+# will ignore punctuation and other special characters when considering whether a word is valid.
+
 class CiphertextMessage(Message):
     def __init__(self, text):
         '''
@@ -33,16 +54,16 @@ class CiphertextMessage(Message):
         max_count = 0
         ans = ()
 
-        for s in range(0, 27):
+        for s in range(0, 26):
             message_text_decrypted = super().apply_shift(s)
             words_in_ciphertext = message_text_decrypted.split(' ')
 
             for word in words_in_ciphertext:
-                if is_word(WORDLIST_FILENAME, word):
+                if is_word(self.valid_words, word):
                     count_of_valid_words += 1
 
             if count_of_valid_words > max_count:
                 max_count = count_of_valid_words
-                ans = (max_count, message_text_decrypted)
+                ans = (s, message_text_decrypted)
 
         return ans
