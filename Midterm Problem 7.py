0be2b02ed47a111b1023f2b92a0f13596b56228a
Flash Wang <phynew0111@gmail.com>
2018-08-30 03:13:02 +0800

    Update Midterm Problem 7.py

--- Midterm Problem 7.py
+++ Midterm Problem 7.py
@@ -31,3 +31,36 @@
 # After you define your function, make a function call to run_satisfiesF(L, satisfiesF). 
 # Do not define f or run_satisfiesF. Do not leave any debugging print statements.
 
+def f(s):
+    return 'a' in s
+
+def satisfiesF(L):
+    """
+    Assumes L is a list of strings
+    Assume function f is already defined for you and it maps a string to a Boolean
+    Mutates L such that it contains all of the strings, s, originally in L such
+            that f(s) returns True, and no other elements. Remaining elements in L
+            should be in the same order.
+    Returns the length of L after mutation
+    """
+    # Your function implementation here
+    if len(L) == 0:
+        return 0
+
+    else:
+        for item in L[:]:  #runs through copy of list so as not to skip place of char removed
+            if not f(item):
+                L.remove(item)
+
+        return len(L)
+
+run_satisfiesF(L, satisfiesF)
+
+L = ['a', 'b', 'a']
+print(satisfiesF(L))
+
+print(L)
+
+K = []
+print(satisfiesF(K))
+print(K)
