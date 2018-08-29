9a4aad5e44681c4798055fe5a47243c972fb9e9f
Flash Wang <phynew0111@gmail.com>
2018-08-30 01:41:43 +0800

    Create Midterm Problem 6.py

---
+++ Midterm Problem 6.py
@@ -0,0 +1,15 @@
+# Problem 6
+# 0.0/20.0 points (graded)
+# Implement a function that meets the specifications below.
+
+# def max_val(t): 
+#     """ t, tuple or list
+#         Each element of t is either an int, a tuple, or a list
+#         No tuple or list is empty
+#         Returns the maximum int in t or (recursively) in an element of t """ 
+#     # Your code here
+# For example,
+
+# max_val((5, (1,2), [[1],[2]])) returns 5.
+# max_val((5, (1,2), [[1],[9]])) returns 9.
+# Paste your entire function, including the definition, in the box below. Do not leave any debugging print statements.
